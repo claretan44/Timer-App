da70b5a879709df1e000f1a1b6c7044d16a80274
cfsyj <cfsyjuco@gmail.com>
2025-03-10 23:37:20 +0800

    Created a dispatcher timer object and a SecondsElapsed class that gets updated every second and can convert the seconds to hh:mm:ss format

--- Timer App/MainWindow.xaml.cs
+++ Timer App/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Timer_App
 {
@@ -16,9 +17,51 @@ namespace Timer_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Initialize the seconds elapsed object
+        private SecondsElapsed secondsElapsed = new SecondsElapsed();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initialize the timer control
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+            // Set the time between "ticks"
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+
+            // Subscribe to the tick event (add a function that occurs when it ticks)
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            secondsElapsed.increment();
+            timeDisplay.Text = secondsElapsed.secondsFormatted();
+        }
+    }
+
+    public class SecondsElapsed
+    {
+        long seconds = 0;
+
+        public void increment()
+        {
+            seconds += 1;
+        }
+
+        public void reset()
+        {
+            seconds = 0;
+        }
+
+        public string secondsFormatted()
+        {
+            TimeSpan span = TimeSpan.FromSeconds(seconds);
+
+            return span.ToString("c");
         }
     }
 }
